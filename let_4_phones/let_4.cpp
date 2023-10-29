2516c64a55a7723fb90875f1bb950684089b0f6d
k0ff1l <kostyarusskiyps@gmail.com>
2023-10-29 22:52:30 +0300

    fixed unnamed namespace in header

--- let_4_phones/let_4.cpp
+++ let_4_phones/let_4.cpp
@@ -14,7 +14,7 @@ int phones() {
     std::cout << "</MobilePhones>" << std::endl;
     return 0;
   }
-  ::MobilePhone *phones = new ::MobilePhone[count];  // вполне XML::MobilePhone перед *phones заменяемо на auto.
+  bmstu::MobilePhone *phones = new bmstu::MobilePhone[count];  // вполне XML::MobilePhone перед *phones заменяемо на auto.
   for (size_t i = 0; i < count; ++i) {
     std::cin >> phones[i].weight;
     std::cin >> phones[i].is_broken;
