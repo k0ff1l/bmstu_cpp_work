d0b386c8affadaacf171ad3966d2f439bd319ef3
k0ff1l <kostyarusskiyps@gmail.com>
2023-10-30 09:00:30 +0300

    prepared files for hw_3_vector

---
+++ hw_3_vector/hw_3_vector.h
@@ -0,0 +1,8 @@
+//
+// Created by fleurs on 30.10.23.
+//
+
+#ifndef HW_3_VECTOR_VECTOR_H_
+#define HW_3_VECTOR_VECTOR_H_
+
+#endif  // HW_3_VECTOR_VECTOR_H_
---
+++ hw_3_vector/hw_3_vector_test.cpp
@@ -0,0 +1,12 @@
+//
+// Created by fleurs on 30.10.23.
+//
+
+#include <gtest/gtest.h>
+#include <iostream>
+#include <vector>
+#include <utility>
+
+TEST(hw_3_vector_test, true){
+  ASSERT_TRUE(true);
+}
