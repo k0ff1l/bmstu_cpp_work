8cbd2d9b8debd882ecb9d0433ffaa6b6b428fc67
k0ff1l <filka@workmgmt.ru>
2023-11-10 09:56:48 +0300

    fixes for script

--- scripts/codestyle.sh
+++ scripts/codestyle.sh
@@ -7,6 +7,6 @@ set -e
 declare -r FILTER=-build/c++23,-runtime/references,\
 -whitespace/braces,-whitespace/indent,-whitespace/comments,-build/include_order,-build/include_subdir
 
-find ./let_*/ ./hw_*/ -name "*.cpp" -or -name "*.hpp" -or -name ".h" | xargs python3 -m cpplint --filter=$FILTER
+grep -E -r --include=\*.{cpp,h,hpp} -L 'cmake-build-debug' ./let_* ./hw_* | xargs python3 -m cpplint --linelength=120 --filter=${FILTER} --output=vs7
 
 popd
