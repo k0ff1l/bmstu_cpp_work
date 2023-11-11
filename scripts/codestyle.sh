5981afe857c0ca21342d5c412a75b19d73d41afe
k0ff1l <filka@workmgmt.ru>
2023-11-11 11:39:16 +0300

    some fixes for script

--- scripts/codestyle.sh
+++ scripts/codestyle.sh
@@ -4,7 +4,7 @@ pushd $(git rev-parse --show-toplevel)
 
 set -e
 
-declare -r FILTER=-build/include_subdir,-legal/copyright
+declare -r FILTER=-build/include_subdir,-legal/copyright,-build/include_what_you_use
 
 grep -E -r --include=\*.{cpp,h,hpp} -L 'cmake-build-debug' ./let_* ./hw_* ./sand* | xargs python3 -m cpplint --linelength=120 --filter=$FILTER --output=vs7 | grep -v "Done processing"
 
