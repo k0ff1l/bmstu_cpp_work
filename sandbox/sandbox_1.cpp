422d8672316ac9e5f57ecaf9f573deaa660779a6
k0ff1l <filka@workmgmt.ru>
2023-11-20 10:49:42 +0300

    commented all sandbox.

--- sandbox/sandbox_1.cpp
+++ sandbox/sandbox_1.cpp
@@ -3,13 +3,17 @@
 #include <iostream>
 #include <string>
 
+// int main() {
+//  std::string str = "str";
+//  std::string str2 = "other";
+//  str2 = std::move(str);
+//  std::cout << str.c_str() << std::endl;
+//  if (str == "") {
+//    std::cout << "True";
+//  }
+//  return 0;
+//}
+
 int main() {
-  std::string str = "str";
-  std::string str2 = "other";
-  str2 = std::move(str);
-  std::cout << str.c_str() << std::endl;
-  if (str == "") {
-    std::cout << "True";
-  }
   return 0;
 }
