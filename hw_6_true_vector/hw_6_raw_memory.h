ea9397f2d078847ba741f1ee99591ba92c986b3d
k0ff1l <filka@workmgmt.ru>
2024-02-15 12:01:54 +0300

    seminar ended

--- hw_6_true_vector/hw_6_raw_memory.h
+++ hw_6_true_vector/hw_6_raw_memory.h
@@ -2,6 +2,8 @@
 
 #pragma once
 
+#include <memory>
+
 namespace bmstu {
 template<typename Z>
 class raw_memory {
@@ -11,16 +13,48 @@ class raw_memory {
     raw_memory(size_t size) : buffer_(allocate_(size)), capacity_(size) {
     }
 
+    ~raw_memory() { deallocate_(buffer_); }
+
     raw_memory(const raw_memory& other) = delete;
     // мы не хотим копировать указатель на память одну и ту же, вдруг один удалится указатель, а второй - нет, будет UB.
 
     raw_memory& operator=(const raw_memory& other) = delete;
 
-    raw_memory(const raw_memory&& other);
+    raw_memory(const raw_memory&& other): capacity_(
+                                            std::exchange(other.capacity_, 0)),
+                                          buffer_(
+                                            std::exchange(
+                                              other.buffer_,
+                                              nullptr)) {
+    };
 
-    ~raw_memory() { deallocate_(buffer_); }
+    raw_memory& operator=(raw_memory&& other) {
+      if (this != &other) {
+        deallocate_(buffer_);
+        buffer_ = std::exchange(other.buffer_, nullptr);
+        // buffer = other.buffer_, other.buffer_ = nullptr
+        capacity_ = std::exchange(other.capacity_, 0);
+      }
+      return *this;
+    }
+
+    Z& operator[](size_t index) noexcept {
+      assert(index < capacity_);
+      return buffer_[index];
+    }
+
+    const Z& operator[](size_t index) const noexcept {
+      assert(index < capacity_);
+      return const_cast<raw_memory>(*this)[index];
+      // todo: возможно нужен raw_memory&
+    }
 
   private:
+    void swap(raw_memory& other) noexcept {
+      std::swap(buffer_, other.buffer_);
+      std::swap(capacity_, other.capacity_);
+    }
+
     // нужны методы, которые аллоцируют и деаллоцируют сырую память.
     static Z* allocate_(size_t size) {
       return size != 0
