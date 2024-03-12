8cb5a8a51ceaa0bea00d079a85a927d11a6e7795
k0ff1l <filka@workmgmt.ru>
2024-03-12 20:49:05 +0300

    some vector additions on DISTANT

--- vector_hw/hw_6_tests.cpp
+++ vector_hw/hw_6_tests.cpp
@@ -19,6 +19,39 @@ class WithoutDefaultConstructor {
     int number;
 };
 
-TEST(TrueVector, test1) {
+class TestClass {
+  public:  // NOLINT
+    TestClass() {
+      std::cout << "alloc def\n";
+      ++cnt;
+      if (cnt == 10) throw std::runtime_error("created 10 testclass species \n");
+    }
+
+    TestClass(int z) : z(z) {
+      std::cout << "alloc with num\n";
+      ++cnt;
+      if (cnt == 10) throw std::runtime_error("created 10 testclass species");
+    }
+
+    ~TestClass() {
+      std::cout << "dealloc\n";
+    }
+  private:
+    int z;
+    static size_t cnt;
+};
+
+size_t TestClass::cnt = 0;
+
+TEST(Vector, test1) {
   bmstu::raw_memory<WithoutDefaultConstructor> memory(10);
+}
+
+TEST(Vector, test2) {
+  try {
+    bmstu::vector<TestClass> a(9);
+    TestClass b(10);
+  } catch (std::runtime_error& e) {
+    std::cout << "catched exception: " << e.what() << "\n";
+  }
 }
--- vector_hw/hw_6_true_vector.h
+++ vector_hw/hw_6_true_vector.h
@@ -2,8 +2,33 @@
 
 #pragma once
 
+#include <cassert>
+#include <memory>
+#include "hw_6_raw_memory.h"
+
 namespace bmstu {
-template<typename Z>
+template<typename T>
 class vector {
+  public:
+    vector() = default;
+
+    vector(size_t size) : data_(size), size_(size) {
+      size_t pos = 0;
+      try {
+        for (auto i = 0; i < data_.capacity(); ++i) {
+          ++pos;
+          ::new(const_cast<void *>(static_cast<const volatile void *>(std::addressof(*(data_.buffer() + i))))) T();
+        }
+      } catch (...) {
+        for (auto i = 0; i < pos; ++i) {
+          std::destroy_at(std::addressof(*(data_.buffer() + i)));
+          std::cout << "wow exception here." << std::endl;
+          throw;
+        }
+      }
+    }
+  private:
+    raw_memory<T> data_;
+    size_t size_ = 0;
 };
 }  // namespace bmstu
